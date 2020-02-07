a3c9cf4ecc064259efe7f2584e5359d21b5e5a6f
Christian Diaz <cdiaz8369@hotmail.com>
2020-02-07 16:18:11 -0500

    Completed Challenge 1

---
+++ Assets/Challenge1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Challenge1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // DUring the final section on the video, we were challenged to have the console print out our name instead of "Hello World"
+        print("CurlyHairedHero says Hi.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
